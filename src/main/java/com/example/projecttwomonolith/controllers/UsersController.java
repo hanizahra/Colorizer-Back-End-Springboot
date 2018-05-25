967ac6584f1fab15784df4736fd65acb013267c4
hanizahra <hanizahra@gmail.com>
2018-05-25 14:35:50 -0400

    got post to work, working on add

--- src/main/java/com/example/projecttwomonolith/controllers/UsersController.java
+++ src/main/java/com/example/projecttwomonolith/controllers/UsersController.java
@@ -15,27 +15,32 @@ public class UsersController {
     @Autowired
     private UserRepository userRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/users")
     public Iterable<User> findAllUsers() {
         return userRepository.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/users/{userId}")
     public Optional<User> findUserById(@PathVariable Long userId) {
         return userRepository.findById(userId);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/users/{userId}")
     public HttpStatus deleteUserById(@PathVariable Long userId) {
         userRepository.deleteById(userId);
         return HttpStatus.OK;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/users")
     public User createNewUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PatchMapping("/users/{userId}")
     public User updateUserById(@PathVariable Long userId, @RequestBody User userRequest) {
 
